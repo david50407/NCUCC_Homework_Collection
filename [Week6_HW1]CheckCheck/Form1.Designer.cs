1c413c8cee9b925af9590ebb09d0ec916c1148b6
David Kuo <me@davy.tw>
2013-11-22 22:57:51 +0800

    Make form un-resizable

--- [Week6_HW1]CheckCheck/Form1.Designer.cs
+++ [Week6_HW1]CheckCheck/Form1.Designer.cs
@@ -208,6 +208,8 @@
             this.Controls.Add(this.button_Start);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "CheckCheck";
             this.Load += new System.EventHandler(this.Form1_Load);
