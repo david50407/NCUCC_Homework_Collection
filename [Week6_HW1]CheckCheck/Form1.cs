e9d0addb16dadc287eb2b8e326df3fea7981bb94
David Kuo <me@davy.tw>
2013-11-22 22:48:41 +0800

    First Commit

---
+++ [Week6_HW1]CheckCheck/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Week6_HW1_CheckCheck
+{
+    public partial class Form1 : Form
+    {
+        private bool bCheckChecking = false;
+        private int iCheckCheckingTime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (CheckBox ch in this.fLP_checkcheck.Controls)
+            {
+                ch.Checked = false;
+                ch.Enabled = false;
+            }
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            if (bCheckChecking)
+                return;
+
+            bCheckChecking = true;
+            this.button_Start.Text = "5";
+            foreach (CheckBox ch in this.fLP_checkcheck.Controls)
+            {
+                ch.Checked = false;
+                ch.Enabled = true;
+            }
+            this.timer_checkcheck.Start();
+        }
+
+        private void timer_checkcheck_Tick(object sender, EventArgs e)
+        {
+            ++iCheckCheckingTime;
+            this.button_Start.Text = (5 - iCheckCheckingTime).ToString();
+            if (iCheckCheckingTime < 5)
+                return;
+
+            this.timer_checkcheck.Stop();
+            int iCheckCheckCount = 0;
+            foreach (CheckBox ch in this.fLP_checkcheck.Controls)
+                if (ch.Checked)
+                    ++iCheckCheckCount;
+            MessageBox.Show(String.Format("你得到 {0} 分 >.0", iCheckCheckCount), "時間到~", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            foreach (CheckBox ch in this.fLP_checkcheck.Controls)
+            {
+                ch.Enabled = false;
+            }
+            this.button_Start.Text = "開始嚕~";
+            bCheckChecking = false;
+        }
+    }
+}
