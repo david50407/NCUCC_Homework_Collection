8707d9fa71e8ff5c1a174d28fdd87213b9a89fcd
David Kuo <me@davy.tw>
2013-11-28 00:03:41 +0800

    Initial Commit

---
+++ [Week7_HW1]IP_List/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Week7_HW1_IP_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            refreshIPList();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            refreshIPList();
+        }
+
+        private void refreshIPList()
+        {
+            this.listBoxIPs.Items.Clear();
+            NetworkInterface[] networkCards = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface networkCard in networkCards)
+            {
+                IPInterfaceProperties IPinfo = networkCard.GetIPProperties();
+                UnicastIPAddressInformationCollection IPAddressInfos = IPinfo.UnicastAddresses;
+                foreach (UnicastIPAddressInformation IPAddressInfo in IPAddressInfos)
+                    if (IPAddressInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                        this.listBoxIPs.Items.Add((IPAddressInfo.Address.ToString()));
+            }
+        }
+    }
+}
